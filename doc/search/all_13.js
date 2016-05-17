fbbbec555ed40d5ba26a1279999017d9cfd07666
Daniel Verkamp <daniel.verkamp@intel.com>
2016-05-17 10:49:45 -0700

    gh-pages/doc: update to 59ae518
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- doc/search/all_13.js
+++ doc/search/all_13.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['unvmcap',['unvmcap',['../structspdk__nvme__ctrlr__data.html#aef21c15160a4556da6b2c2dbfee5749f',1,'spdk_nvme_ctrlr_data']]]
+  ['unvmcap',['unvmcap',['../structspdk__nvme__ctrlr__data.html#aef21c15160a4556da6b2c2dbfee5749f',1,'spdk_nvme_ctrlr_data']]],
+  ['use_5fcmb_5fsqs',['use_cmb_sqs',['../structspdk__nvme__ctrlr__opts.html#a69f7c6f3a946ed379a7b8b63faa70a48',1,'spdk_nvme_ctrlr_opts']]]
 ];
